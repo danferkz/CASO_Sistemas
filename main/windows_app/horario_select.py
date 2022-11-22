29ac211f43ff6a2f4feb0f4f91bf1ff29f863831
Franc02306 <francomunoz230602@gmail.com>
2022-11-22 16:09:02 -0500

    ok

--- main/windows_app/horario_select.py
+++ main/windows_app/horario_select.py
@@ -17,27 +17,23 @@ def horario(root, mainFrame):
     mainFrame.config(width = "380", height = "676")
     mainFrame.pack()
     my_path = readfiles.Route()
-
     global logo, vaerDropBox
-
     logo = ImageTk.PhotoImage(Image.open(my_path + "\main\images\HORARIO1.png"))
-
     Label(mainFrame, image = logo).place(relx = 0, rely = 0)
     
 ############################## 
+
     categoriesNUMEROSvar = ['Dia', 'Tarde', 'Noche']
-    Label(mainFrame, text = "SELECCIÓN DE FASES DEL DÍA : ", relief= "flat", bg="#EFFBFB", font=("Inter", 10)).place(x = 50, y = 100)
+    Label(mainFrame, text = "SELECCIÓN DE FASES DEL DÍA : ", relief= "flat", bg="#EFFBFB", font=("Inter", 10)).place(x = 35, y = 125)
     vaerDropBox= ttk.Combobox(mainFrame,font=("Inter", 16,"bold"),width = 10)
     vaerDropBox.set("Fases del Día")
-
     vaerDropBox.set("Dia")
     vaerDropBox["values"]  = categoriesNUMEROSvar
-    vaerDropBox.place(x = 100, y = 350)
+    vaerDropBox.place(x = 35, y = 160)
     
-    
-    Button(mainFrame, text = "Continuar", width = 25,relief="flat",font=("Inter", 12,"bold"),bg="#000000",fg="#ffffff", command = lambda: Selec_Rooty()).place(x = 100, y = 400)
+##############################
 
-    Button(mainFrame, text = "Continuar", width = 25,relief="flat",font=("Inter", 12,"bold"),bg="#000000",fg="#ffffff", command = lambda: Selec_Rooty(root, mainFrame)).place(x = 100, y = 400)
+    Button(mainFrame, text = "Continuar", width = 25,relief="flat",font=("Inter", 12,"bold"),bg="#81F781",fg="#000000", command = lambda: Selec_Rooty(root, mainFrame)).place(x = 110, y = 450)
     
 
 
@@ -51,4 +47,3 @@ def Selec_Rooty(root, mainFrame):
         tarde.horaTARDE(root, mainFrame)
     else:
         noche.horaNOCHE(root, mainFrame)
-    
