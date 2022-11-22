942e56405f305612a525b2bd0255eef6fcef3be8
danferkz <gdanfer07@gmail.com>
2022-11-22 12:37:05 -0500

    corregido

--- main/windows_app/horario_select.py
+++ main/windows_app/horario_select.py
@@ -3,44 +3,45 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox as MessageBox
 import windows_app.LOG as log
+import windows_app.hora_dia as dia
 import helpers.readfiles as readfiles
 from PIL import Image, ImageTk
 from tkinter import Tk, Label, Button,Entry, Frame, END
 
 
-def horario(root, mainframe):
+def horario(root, mainFrame):
     root.title("Horarios de líneas de tren")
 
-    mainframe.destroy()
-    mainframe = Frame()
-    mainframe.config(width = "380", height = "676")
-    mainframe.pack()
+    mainFrame.destroy()
+    mainFrame = Frame()
+    mainFrame.config(width = "380", height = "676")
+    mainFrame.pack()
     my_path = readfiles.Route()
 
     global logo, vaerDropBox
 
     logo = ImageTk.PhotoImage(Image.open(my_path + "\main\images\HORARIO_BIENVENIDA.png"))
 
-    Label(mainframe, image = logo).place(relx = 0, rely = 0)
+    Label(mainFrame, image = logo).place(relx = 0, rely = 0)
 ############################## 
     categoriesNUMEROSvar = ['Dia', 'Tarde', 'Noche']
-    Label(mainframe, text = "Seleccion de fase de Dia", relief= "flat", bg="#767574", font=("Inter", 10)).place(x = 100, y = 300)
-    vaerDropBox= ttk.Combobox(mainframe,font=("Inter", 16,"bold"),width = 10)
+    Label(mainFrame, text = "Seleccion de fase de Dia", relief= "flat", bg="#767574", font=("Inter", 10)).place(x = 100, y = 300)
+    vaerDropBox= ttk.Combobox(mainFrame,font=("Inter", 16,"bold"),width = 10)
     vaerDropBox.set("variables")
     vaerDropBox["values"]  = categoriesNUMEROSvar
     vaerDropBox.place(x = 100, y = 350)
     
     
-    Button(mainframe, text = "FORMAR TABLAS", width = 25,relief="flat",font=("Inter", 12,"bold"),bg="#000000",fg="#ffffff", command = lambda: Selec_Rooty()).place(x = 100, y = 400)
+    Button(mainFrame, text = "FORMAR TABLAS", width = 25,relief="flat",font=("Inter", 12,"bold"),bg="#000000",fg="#ffffff", command = lambda: Selec_Rooty(root, mainFrame)).place(x = 100, y = 400)
     
 
 
 #RUTAS
-def Selec_Rooty():
+def Selec_Rooty(root, mainFrame):
     a = vaerDropBox.get()
     
     if a == 'Dia':
-        "dos_cua.pantalla4x4(root, mainFrame)"
+        dia.horaMAÑANA(root, mainFrame)
     elif a == 'Tarde':
         "dos_cua.pantalla4x4(root, mainFrame)"
     else:
