c9996c48a10224f597b39b42c7cc9567055cd8f9
danferkz <gdanfer07@gmail.com>
2022-11-22 16:08:06 -0500

    ya

--- main/windows_app/hora_dia.py
+++ main/windows_app/hora_dia.py
@@ -1,6 +1,9 @@
 from tkinter import *
+from tkinter import ttk
 import windows_app.RECARGA as reca
 import windows_app.RECARGA as recar_train
+import windows_app.horario_tarde as tarde
+import windows_app.horario_noche as noche
 import helpers.readfiles as readfiles
 import windows_app.hora_dia as dia
 from PIL import Image, ImageTk
@@ -16,8 +19,35 @@ def horaMAÑANA(root, mainFrame):
     mainFrame.config(width = "380", height = "676")
     mainFrame.pack()
     my_path = readfiles.Route()
-    global logo
+    global logo, vaerDropBox
     logo = ImageTk.PhotoImage(Image.open(my_path + "\main\images\HORARIO1.png"))
     Label(mainFrame, image = logo).place(relx = 0, rely = 0)
+    
+    categoriesNUMEROSvar = ['Dia', 'Tarde', 'Noche']
+    Label(mainFrame, text = "SELECCIÓN DE FASES DEL DÍA : ", relief= "flat", bg="#EFFBFB", font=("Inter", 10)).place(x = 50, y = 100)
+    vaerDropBox= ttk.Combobox(mainFrame,font=("Inter", 16,"bold"),width = 10)
+    vaerDropBox.set("Fases del Día")
+
+    vaerDropBox.set("Dia")
+    vaerDropBox["values"]  = categoriesNUMEROSvar
+    vaerDropBox.place(x = 100, y = 350)
+    
+    
+    Button(mainFrame, text = "Continuar", width = 25,relief="flat",font=("Inter", 12,"bold"),bg="#000000",fg="#ffffff", command = lambda: Selec_Rooty()).place(x = 100, y = 400)
+
+    Button(mainFrame, text = "Continuar", width = 25,relief="flat",font=("Inter", 12,"bold"),bg="#000000",fg="#ffffff", command = lambda: Selec_Rooty(root, mainFrame)).place(x = 100, y = 400)
+    
+
+
+#RUTAS
+def Selec_Rooty(root, mainFrame):
+    a = vaerDropBox.get()
+    
+    if a == 'Dia':
+        dia.horaMAÑANA(root, mainFrame)
+    elif a == 'Tarde':
+        tarde.horaTARDE(root, mainFrame)
+    else:
+        noche.horaNOCHE(root, mainFrame)
 
 
--- main/windows_app/horario_select.py
+++ main/windows_app/horario_select.py
@@ -5,12 +5,13 @@ import windows_app.LOG as log
 import windows_app.hora_dia as dia
 import windows_app.horario_tarde as tarde
 import windows_app.horario_noche as noche
+import windows_app.hora_dia as dia
 import helpers.readfiles as readfiles
 from PIL import Image, ImageTk
 from tkinter import Tk, Label, Button,Entry, Frame, END
 
 
-def horario(root, mainFrame):
+def horarioBien(root, mainFrame):
     root.title("Horarios de líneas de tren")
     mainFrame.destroy()
     mainFrame = Frame()
@@ -20,35 +21,12 @@ def horario(root, mainFrame):
 
     global logo, vaerDropBox
 
-    logo = ImageTk.PhotoImage(Image.open(my_path + "\main\images\HORARIO1.png"))
+    logo = ImageTk.PhotoImage(Image.open(my_path + "\main\images\HORARIO_BIENVENIDA.png"))
 
     Label(mainFrame, image = logo).place(relx = 0, rely = 0)
     
 ############################## 
-    categoriesNUMEROSvar = ['Dia', 'Tarde', 'Noche']
-    Label(mainFrame, text = "SELECCIÓN DE FASES DEL DÍA : ", relief= "flat", bg="#EFFBFB", font=("Inter", 10)).place(x = 50, y = 100)
-    vaerDropBox= ttk.Combobox(mainFrame,font=("Inter", 16,"bold"),width = 10)
-    vaerDropBox.set("Fases del Día")
-
-    vaerDropBox.set("Dia")
-    vaerDropBox["values"]  = categoriesNUMEROSvar
-    vaerDropBox.place(x = 100, y = 350)
-    
-    
-    Button(mainFrame, text = "Continuar", width = 25,relief="flat",font=("Inter", 12,"bold"),bg="#000000",fg="#ffffff", command = lambda: Selec_Rooty()).place(x = 100, y = 400)
-
-    Button(mainFrame, text = "Continuar", width = 25,relief="flat",font=("Inter", 12,"bold"),bg="#000000",fg="#ffffff", command = lambda: Selec_Rooty(root, mainFrame)).place(x = 100, y = 400)
     
 
-
-#RUTAS
-def Selec_Rooty(root, mainFrame):
-    a = vaerDropBox.get()
-    
-    if a == 'Dia':
-        dia.horaMAÑANA(root, mainFrame)
-    elif a == 'Tarde':
-        tarde.horaTARDE(root, mainFrame)
-    else:
-        noche.horaNOCHE(root, mainFrame)
+    Button(mainFrame, text = "Continuar", width = 25,relief="flat",font=("Inter", 12,"bold"),bg="#000000",fg="#ffffff", command = lambda: dia.horaMAÑANA(root, mainFrame)).place(x = 100, y = 400)
     
