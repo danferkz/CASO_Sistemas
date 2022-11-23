58e823aadbb3ae45bed1cf8bd39cde730d2b2242
danferkz <gdanfer07@gmail.com>
2022-11-22 20:29:10 -0500

    terminado

--- main/windows_app/Notifica_c.py
+++ main/windows_app/Notifica_c.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import windows_app.RECARGA as reca
+import windows_app.HOME as home
 import windows_app.ENVIONO as envia
 import helpers.readfiles as readfiles
 from PIL import Image, ImageTk
@@ -20,6 +21,11 @@ def VisNo_otifica(root, mainFrame):
     Label(mainFrame, image = logo).place(relx = 0, rely = 0)
 
 
-    
+    Button(mainFrame, text = "Home", width = 7, bg="#03989e", relief="flat",font=("Inter", 13,"bold"), command = lambda: home.HOM(root, mainFrame)).place(x = 253, y = 613)
 
-    Button(mainFrame, text = "ENVIAR Incidencia????", width = 10, bg = "#03989e", relief="flat",font=("Inter", 15,"bold"), command = lambda: envia.Enviar_NOti(root, mainFrame)).place(x = 100, y = 490)
+    Button(mainFrame, text = "ENVIAR Incidencia????", width = 18, bg = "#03989e", relief="flat",font=("Inter", 15,"bold"), command = lambda: envia.Enviar_NOti(root, mainFrame)).place(x = 80, y = 425)
+    
+    Label(mainFrame, text = "SILENCIAR ", relief= "flat", bg="#EFFBFB", font=("Inter", 10)).place(x = 100, y = 528)
+    
+    visa = StringVar()
+    Checkbutton(mainFrame, textvariable = visa, bg="#FFFFFF",font=("Inter", 22), relief="flat").place(x = 47, y = 528)
